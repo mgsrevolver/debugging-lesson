41d1f53f2596811cf963fc3fc9db539df9a45611
mgsrevolver <clayreimus@gmail.com>
2022-05-17 16:44:24 -0400

    fixing bugs

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-import styles from './Button.module.css';
+import styles from './Button.module.css'
 
-const Button = props => {
+const Button = (props) => {
   return (
     <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
